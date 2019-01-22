35390ed36b3dc8b9b93cf584f1a22ee07b8a539b
TUNG-LIN, YANG <erza0211064@gmail.com>
2019-01-21 20:26:05 -0800

    define green color

---
+++ defineGreenBarrel.py
@@ -0,0 +1,34 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+from roipoly import RoiPoly as ro
+import pickle
+import os
+
+def display(img):
+    cv2.imshow('image',img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows
+Green_trainingData = [[],[],[]]
+image_path = os.listdir('C:/Users/TonyY/AppData/Local/Programs/Python/Python37-32/code/276HW1/trainset')
+for image in image_path:
+    #Read image
+    img1 = cv2.imread('.\\trainset\\'+image)
+    img2 = cv2.cvtColor(img1,cv2.COLOR_RGB2BGR)
+    #show image and define region of interest
+    plt.imshow(img2, interpolation='nearest')
+    plt.show(block=False)
+    roi = ro(color='r')
+    #return an np array where the region you draw is True and others are false
+    mask = roi.get_mask(img2)
+    a = np.where(mask == True)
+    img_tcrcb = cv2.cvtColor(img1,cv2.COLOR_RGB2YCR_CB)
+
+
+    for i in range(len(a[0])):
+        Green_trainingData[0].append(img_tcrcb[a[0][i],a[1][i],0])
+        Green_trainingData[1].append(img_tcrcb[a[0][i],a[1][i],1])
+        Green_trainingData[2].append(img_tcrcb[a[0][i],a[1][i],2])
+file = open("GreenData","wb")
+pickle.dump(Green_trainingData,file)
+file.close()
